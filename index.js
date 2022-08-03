290aa86b25e3a62618007887a6153ab4bd87576a
curse <58047023+cursecodes@users.noreply.github.com>
2022-08-02 22:25:30 -0600

    Remove errorExt as useless in research.

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 /**
  * @param { Promise } promise
- * @param { Object= } errorExt - Additional Information you can pass to the err object
  * @return { Promise }
  */
-function to(promise, errorExt) {
+function to(promise) {
   return promise
     .then((data) => [null, data])
     .catch((err) => {
-      if (errorExt) {
-        const parsedError = Object.assign({}, err, errorExt);
-        return [parsedError, undefined];
-      }
-
       return [err, undefined];
     });
 }
